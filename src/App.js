968e91fa214f8fd6259d9a2bfded813bb5b06f19
Priyanka-BN11 <78422557+Priyanka-BN11@users.noreply.github.com>
2022-10-07 15:52:38 +0200

    google site verification added in index.html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Events from './events';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Meet app</h1>
+      <Events/>
     </div>
   );
 }
