c297ff05464030945ae0a72292dd4b176e0950f4
Priyanka-BN11 <78422557+Priyanka-BN11@users.noreply.github.com>
2022-10-15 21:30:28 +0200

    Scenario 1,2,3 updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import './App.css';
 import NumberOfEvents from './NumberOfEvents';
+import { mockData } from "./mock-data";
+import { extractLocations } from './api';
 
-function App() {
+class App extends Component {
+  state = {
+    events: mockData,
+    locations: extractLocations(mockData),
+  }
+
+  
+  
+
+
+  render() {
   return (
     <div className="App">
-      <h1>Meet app</h1>
-      <CitySearch/>
-      <EventList/>
-      <NumberOfEvents/>
+      <CitySearch locations={this.state.locations} />
+      <NumberOfEvents />
+      <EventList events={this.state.events}/>
     </div>
   );
+}
 }
 
 export default App;
