172ba31404f452ceebbe0c1f28e4e29d11ca169d
Priyanka-BN11 <78422557+Priyanka-BN11@users.noreply.github.com>
2022-10-19 19:58:46 +0200

    integration test 4.4

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
 import React, {Component} from 'react';
+import './nprogress.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import './App.css';
-import NumberOfEvents from './NumberOfEvents';
 import { mockData } from "./mock-data";
-import { extractLocations } from './api';
+import NumberOfEvents from './NumberOfEvents';
+import { extractLocations, getEvents} from './api';
+
+
 
 class App extends Component {
   state = {
-    events: mockData,
-    locations: extractLocations(mockData),
+    events: [],
+    locations: []
   }
-
-  
-  
-
-
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+  componentDidMount(){
+    this.mounted = true;
+    getEvents().then((events) => {
+      if(this.mounted){
+      this.setState({events, locations: extractLocations(events)});
+      }
+    });
+  }
+  componentWillUnmount(){
+    this.mounted = false;
+  }
   render() {
   return (
     <div className="App">
-      <CitySearch locations={this.state.locations} />
+      <h1>Meet App</h1>
+   <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
       <NumberOfEvents />
-      <EventList events={this.state.events}/>
+      <EventList events={this.state.events} size='6' sm='3'/>
     </div>
   );
 }
 }
+
 
 export default App;
